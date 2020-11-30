d6f5d01decc71c6538a6412374f1f47576c24d07
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-30 09:11:13 +0530

    Added : Ability to Remove An Employee

---
+++ src/main/java/com/blz/payrolljdbc/EmployeePayrollException.java
@@ -0,0 +1,15 @@
+package com.blz.payrolljdbc;
+
+public class EmployeePayrollException extends Exception {
+
+	enum ExceptionType {
+		DATABASE_EXCEPTION, NO_SUCH_CLASS, COMMIT_FAILED, RESOURCES_NOT_CLOSED_EXCEPTION, CONNECTION_FAILED
+	}
+
+	public ExceptionType type;
+
+	public EmployeePayrollException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
